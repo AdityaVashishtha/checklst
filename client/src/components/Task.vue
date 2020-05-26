51f4071e466ad9049340105e526fb0c159227bd3
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2020-05-27 00:16:35 +0530

    Front end Finished

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,29 +1,59 @@
 <template>
-  <div class="container">
-    <UserWelcomeHeader />
-    <SearchBar />
+  <div class="columns">
+    <div class="column is-two-fifths">
+      <div class="is-hidden-mobile task-page">
+        <AddTask />
+      </div>
+    </div>
+    <div class="column is-half">
+      <div class="task-page">
+        <UserWelcomeHeader />
+        <SearchBar />
+        <HorizontalScrollView />
+        <TaskList />
+        <div class="float-btn is-hidden-desktop">
+          <button class="button is-danger is-large is-rounded">
+            <span class="icon is-small">
+              <i class="fas fa-plus"></i>
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import UserWelcomeHeader from "./UserWelcomeHeader.vue";
 import SearchBar from "./SearchBar";
+import HorizontalScrollView from "./HorizontalScrollView";
+import TaskList from "./TaskList";
+import AddTask from "./AddTask";
 
 export default {
   name: "Task",
   components: {
     UserWelcomeHeader,
-    SearchBar
+    SearchBar,
+    HorizontalScrollView,
+    TaskList,
+    AddTask
   }
 };
 </script>
 
 <style lang="scss">
-.container {
+.task-page {
+  background-color: #fff5f5;
   padding: var(--l-px);
-  max-width: 500px;
-  height: 100vh;
-  border: 1px solid gray;
+  height: calc(100vh-20px);
   border-radius: var(--l-px);
+  margin-left: var(--xl-px);
+  margin-top: var(--xl-px);
+}
+.float-btn {
+  position: fixed;
+  bottom: var(--xl-px);
+  right: var(--xl-px);
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import '@fortawesome/fontawesome-free/js/all';
+import 'hammerjs';
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
